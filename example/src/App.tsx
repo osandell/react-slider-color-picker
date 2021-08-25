3ec5cd754b722a6c1a19caa8eebc5a8812391b20
Olof Sandell <contact@olofsandell.com>
2021-08-25 18:18:56 +0200

    feat: add a box to show the produced color in the example app
    This adds a box below the sliders to demonstrate the produced color. Also setting up emotion ui for
    css-in-js.
    
    re #4

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+import { useState } from 'react'
+
+// @ts-ignore
+import myImagePath from './checker-pattern.png'
 
 import { HueSlider, SaturationSlider, LightnessSlider, AlphaSlider } from 'react-slider-color-picker'
 import './index.css'
@@ -7,6 +12,7 @@ import './index.css'
 import { Color } from '../../src/interfaces'
 
 const App = () => {
+
   const [color, setColor] = useState<Color>({h: 180, s: 100, l: 50, a: 1})
 
   const handleChangeHue = (newHue: string) => {
@@ -25,8 +31,25 @@ const App = () => {
     setColor({...color, a: parseInt(newAlpha) / 100})
   }
 
+  const root = css({
+    width: '400px',
+    height: '100vh',
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  })
+  
+  const colorBox = css({
+    width: '100px',
+    height: '100px',
+    margin: '50px auto 0 auto',
+    background: `linear-gradient(hsla(${color.h}, ${color.s}%, ${color.l}%, ${color.a}), hsla(${color.h}, ${color.s}%, ${color.l}%, ${color.a})), url(${myImagePath})`
+    // `linear-gradient(hsla(${props.color.h}, 100%, 50%, ${props.color.a}),  hsla(${props.color.h}, 100%, 50%, ${props.color.a})), url(${myImagePath})`
+  })
+
   return (
-    <>
+    <div css={root}>
       <HueSlider handleChange={handleChangeHue} color={color} />
       <SaturationSlider handleChange={handleChangeSaturation} color={color} />
       <LightnessSlider handleChange={handleChangeLightness} color={color} />
@@ -34,7 +57,8 @@ const App = () => {
         handleChange={handleChangeAlpha}
         color={color}
       />
-    </>
+      <div css={colorBox}></div>
+    </div>
   )
 }
 
