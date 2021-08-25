b5ddefbefff589752f153436c80104c98ffc265d
Olof Sandell <contact@olofsandell.com>
2021-08-25 15:31:02 +0200

    Updates

---
+++ precache-manifest.2b21c1d86ad86231c0fa4195d5349986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455189a03ac05cb309b6b3b16cfcc505",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3278bba0d050011c4f2c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "4e5f20d7a90146638c8a",
+    "url": "./static/js/2.a1839aa1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a1839aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3278bba0d050011c4f2c",
+    "url": "./static/js/main.0a061c27.chunk.js"
+  },
+  {
+    "revision": "256956b9bd1b052a0307",
+    "url": "./static/js/runtime-main.e4d81c88.js"
+  }
+]);
